3a367f76ed65c731dcd70d718da9c50e29169f80
Victor Vilela <victorsouzavilelasilva@gmail.com>
2023-04-05 13:16:55 -0300

    mensagem

---
+++ API.js
@@ -0,0 +1,22 @@
+
+import axios from 'axios';
+import fetch from 'node-fetch';
+
+
+
+
+//enquanto houver espeaço
+const APIKey = 'RGAPI-3386a2f1-66b7-4bee-989b-0c1e480991fe';
+const name = "É o Dino ne";
+let nome = "";
+
+//faz o request para a API
+var request = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + name + '?api_key=' + APIKey;
+fetch(request).then(response => {
+    nome = response.json();
+}).then(corpo => {
+    const nome = corpo;
+})
+
+console.log(nome);
+
